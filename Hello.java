d49a50045367e462fbb1163b52cfbccfb98b4054
Aishwarya Nair <aishwaryanair11997@gmail.com>
2019-11-05 15:04:24 +0530

    Changed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World");
+
+System.out.println("Welcome to Lti");
+System.out.println("Hello Mumbai");
+System.out.println("Good Morning");
 			}
 }
